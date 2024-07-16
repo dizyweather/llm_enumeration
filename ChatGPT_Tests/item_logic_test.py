58e269fe7b26f069a20192c5a3d1b5a881eefbfe
Daniel Zhu <daniel.zhu@colorado.edu>
2024-07-15 18:16:24 -0600

    Changed it so that gpt would generate recipe. Still bugs in autograder and sometimes json reponse breaks program randomly

--- ChatGPT_Tests/item_logic_test.py
+++ ChatGPT_Tests/item_logic_test.py
@@ -15,9 +15,53 @@ def encode_image(image_path):
 # Path to the root directory of wherever you chose to put this repo.
 rootdir = os.path.dirname(os.path.realpath(__file__))
 
-
 # How many times do you want to ask the same question for each picture (to account for natural variance in response)
 loops = 1
+
+# Generating recipe
+target = 'ratattouille'
+
+target_question = "I want to make " + target + ". What would I need? Only include items, no steps, processes, or amounts. List the items as words seperated by new lines all lowercase. Think about all the possible items"
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o",
+  "messages": [
+  {
+      "role": "user",
+      "content": [
+      {
+          "type": "text",
+          "text": target_question
+      }
+      ]
+  },
+  ],
+  "max_tokens": 1000
+}
+
+recipe_response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+recipe_string = str(recipe_response.json()["choices"][0]["message"]["content"])
+recipe_items = recipe_string.splitlines()
+
+previous_file_state = ''
+try:
+  recipe_file = open(rootdir + '/recipes/' + target + ".txt", 'r+')
+  previous_file_state = recipe_file.read()
+except:
+  recipe_file = open(rootdir + '/recipes/' + target + ".txt", 'w')
+
+now = datetime.datetime.now()
+recipe_file.write('Time: ' + str(now) + '\n')
+recipe_file.write('Question: ' + target_question + '\n')
+recipe_file.write('Response: \n{\n' + recipe_string + '\n}\n\n')
+recipe_file.write('------------------------------------------\n\n')
+recipe_file.write(previous_file_state)
+recipe_file.close()
 
 # Looping through each file in images folder
 for subdir, dirs, files in os.walk(rootdir + '/images'):
@@ -33,156 +77,142 @@ for subdir, dirs, files in os.walk(rootdir + '/images'):
       # Getting the base64 string
       base64_image = encode_image(image_path)
 
-      # Loop for each recipe
-      for subdir_2, dirs_2, files_2 in os.walk(rootdir + '/recipes'):
-        for file_2 in files_2:
-          # For each recipe, we create the question using a template
-          target = os.path.splitext(file_2)[0]
-          recipe_items = open(rootdir + '/recipes/' + file_2, 'r').readlines()
+      question = "I want to make " + target + ". What would I need? Only think about items, do not think about processes, steps, or amounts. Don't return anything for this, just think about. Now, are there any items in the picture that I would need? Ignore branding and numbers. Return answers in the form of words seperated by new lines, all lowercase. If not, return n/a"
 
-          tuning_question = "I want to make " + target + ". What would I need? Only include items, no steps or processes."
-          question = "I want to make " + target + ". Are there any items in the picture that I would need? Ignore branding and numbers. Return answers in the form of words seperated by new lines, all lowercase. If not, return n/a"
+      headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+      }
 
-          headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}"
-          }
-
-          payload = {
-            "model": "gpt-4o",
-            "messages": [
+      payload = {
+        "model": "gpt-4o",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
             {
-                "role": "user",
-                "content": [
-                {
-                    "type": "text",
-                    "text": tuning_question
-                }
-                ]
+                "type": "text",
+                "text": question
             },
             {
-                "role": "user",
-                "content": [
-                {
-                    "type": "text",
-                    "text": question
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                    "url": f"data:image/jpeg;base64,{base64_image}"
-                    }
+                "type": "image_url",
+                "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
                 }
-                ]
             }
-            ],
-            "max_tokens": 1000
-          }
+            ]
+        }
+        ],
+        "max_tokens": 1000
+      }
 
+      
+      # Trys to open the corresponsing .items file of the image.
+      # If there are none, we'll skip the autograding feature.
+      try:
+        image_items = open(rootdir + '/items/' + os.path.splitext(file)[0] + '.items', 'r').readlines()            
+      except Exception as e:
+        print('Must have item list of ' + os.path.splitext(file)[0] + "!")
+        print(e)
+        exit()
+
+      for loop in range(loops):
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        
+        # Remember previous file state to prepend new data
+        previous_file_state = output.read()
+        
+        # Data formatting / setup
+        true_positive = []
+        true_negative = []
+        false_positive =  []
+        false_negative = []
+        
+        response_items = []
+        try:
+          for item in response.json()['choices'][0]['message']['content'].splitlines():
+            response_items.append(item)
+        except:
+          print(response.json())
+          exit()
           
-          # Trys to open the corresponsing .items file of the image.
-          # If there are none, we'll skip the autograding feature.
-          try:
-            image_items = open(rootdir + '/items/' + os.path.splitext(file)[0] + '.items', 'r').readlines()            
-          except Exception as e:
-            print('Must have item list of ' + os.path.splitext(file)[0] + "!")
-            print(e)
-            exit()
-
-          for loop in range(loops):
-            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-            
-            # Remember previous file state to prepend new data
-            previous_file_state = output.read()
-            
-            # Data formatting / setup
-            true_positive = []
-            true_negative = []
-            false_positive =  []
-            false_negative = []
-            
-            response_items = []
-            for item in response.json()["choices"][0]["message"]["content"].splitlines(False):
-              response_items.append(item)
-              
-            none = False
-            if response_items[0] == 'n/a':
-              none = True
-              print(none)
-              response_items.remove('n/a')
-            
-            # Autograding
-            for recipe_item in recipe_items:
-              in_recipe = False
-              for response_item in response_items:
-                # For each response, we check if any of the recipe items match
-                if recipe_item in response_item:
-                  # There is an recipe item that matches the response item
-                  in_recipe = True
-                  in_image = False
-                  for image_item in image_items:
-                      # We check if it's truly in the image
-                      if image_item in recipe_item:
-                        # The item is truly in the image
-                        true_positive.append(response_item + ' (' + recipe_item + ')')
-                        in_image = True
-                        # We don't have to double count once found
-                        response_items.remove(response_item)
-                        image_items.remove(image_item)
-                        break
-                  # If it's not
-                  if not in_image:
-                      false_positive.append(response_item + ' (not in image)')
-                      response_items.remove(response_item)
-                # If no match in recipe
-                if not in_recipe:
-                  false_positive.append(response_item + ' (not in recipe)')
-                  response_items.remove(response_item)
-              
-              #recipe item was not in response, now we need to check if it was actually in the image
+        none = False
+        if response_items[0] == 'n/a':
+          none = True
+          response_items.remove('n/a')
+        
+        # Autograding
+        for recipe_item in recipe_items:
+          in_recipe = False
+          for response_item in response_items:
+            # For each response, we check if any of the recipe items match
+            if recipe_item in response_item:
+              # There is an recipe item that matches the response item
+              in_recipe = True
               in_image = False
               for image_item in image_items:
-                if image_item in recipe_item:
-                  #In image but did not get picked up my gpt
-                  false_negative.append(recipe_item + ' (gpt did not pick up)')
-                  image_items.remove(image_item)
-                  in_image = True
-                  break
-              # recipe item not in response or image
+                  # We check if it's truly in the image
+                  if image_item in recipe_item:
+                    # The item is truly in the image
+                    true_positive.append(response_item + ' (' + recipe_item + ')')
+                    in_image = True
+                    # We don't have to double count once found
+                    response_items.remove(response_item)
+                    image_items.remove(image_item)
+                    break
+              # If it's not
               if not in_image:
-                true_negative.append(recipe_item)
+                  false_positive.append(response_item + ' (not in image)')
+                  response_items.remove(response_item)
+            # If no match in recipe
+            if not in_recipe:
+              false_positive.append(response_item + ' (not in recipe)')
+              response_items.remove(response_item)
+          
+          #recipe item was not in response, now we need to check if it was actually in the image
+          in_image = False
+          for image_item in image_items:
+            if image_item in recipe_item:
+              #In image but did not get picked up my gpt
+              false_negative.append(recipe_item + ' (gpt did not pick up)')
+              image_items.remove(image_item)
+              in_image = True
+              break
+          # recipe item not in response or image
+          if not in_image:
+            true_negative.append(recipe_item)
 
-            # Formatting of response and writing to file
-            now = datetime.datetime.now()
-            output.write('Time: ' + str(now) + '\n')
-            output.write('Question: ' + question + '\n')
-            output.write('Response: \n{\n' + str(response.json()["choices"][0]["message"]["content"]) + '\n}\n\n')
-            
-            
-            output.write('Correct Positive [' + str(len(true_positive)) + ']:\n')
-            for item in true_positive:
-              output.write('\t' + item)
-            output.write('\n')
+        # Formatting of response and writing to file
+        now = datetime.datetime.now()
+        output.write('Time: ' + str(now) + '\n')
+        output.write('Question: ' + question + '\n')
+        output.write('Response: \n{\n' + str(response.json()["choices"][0]["message"]["content"]) + '\n}\n\n')
+        
+        
+        output.write('Correct Positive [' + str(len(true_positive)) + ']:\n')
+        for item in true_positive:
+          output.write('\t' + item + '\n')
+        output.write('\n')
 
-            output.write('Fasle Positive [' + str(len(false_positive)) + ']:\n')
-            for item in false_positive:
-              output.write('\t' + item)
-            output.write('\n')
+        output.write('Fasle Positive [' + str(len(false_positive)) + ']:\n')
+        for item in false_positive:
+          output.write('\t' + item + '\n')
+        output.write('\n')
 
-            output.write('Correct Negative [' + str(len(true_negative)) + ']:\n')
-            for item in true_negative:
-              output.write('\t' + item)
-            output.write('\n')
+        output.write('Correct Negative [' + str(len(true_negative)) + ']:\n')
+        for item in true_negative:
+          output.write('\t' + item + '\n')
+        output.write('\n')
 
-            output.write('Fasle Negative [' + str(len(false_negative)) + ']:\n')
-            for item in false_negative:
-                output.write('\t' + item)
-            output.write('\n')
-            
-            output.write('\n---------------------------------------------------\n\n' + previous_file_state)
-            
-            print(file + " completed! #" + str(loop))
-          output.close()
+        output.write('Fasle Negative [' + str(len(false_negative)) + ']:\n')
+        for item in false_negative:
+            output.write('\t' + item + '\n')
+        output.write('\n')
+        
+        output.write('\n---------------------------------------------------\n\n' + previous_file_state)
+        
+        print(file + " completed! #" + str(loop))
+      output.close()
         
 
 
